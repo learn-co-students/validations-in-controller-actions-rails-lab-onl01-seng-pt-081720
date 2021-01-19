968e27aaafbe60bd590cfa0c0fd4a9774a177d85
Angelica203 <brantleyhomebuyer@gmail.com>
2021-01-19 16:44:54 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    @post = Post.all
+    # @post = Post.all
   end
 
   def edit
